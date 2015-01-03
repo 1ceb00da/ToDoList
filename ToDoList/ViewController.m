8b05d371a6dcff7fd7b7fe5bbac1bebab3df1ed7
Aditya Dhulipala <adhulipa@usc.edu>
2015-01-03 17:29:01 +0530

    Initial commit from local git

---
+++ ToDoList/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ToDoList
+//
+//  Created by Aditya Dhulipala on 1/3/15.
+//  Copyright (c) 2015 Aditya Dhulipala. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
